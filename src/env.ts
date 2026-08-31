6ece5dfe17149fc6fba7c520a9925a12a5d721a5
Bob McAleavey <bobmcaleavey@gmail.com>
2026-08-31 13:24:40 -0400

    fix: change variable build strategy

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,13 @@
+import { building } from '$app/env';
 import { defineEnvVars } from '@sveltejs/kit/env';
+import * as v from 'valibot';
 
 export const variables = defineEnvVars({
-	MONGODB_DB: {},
-	MONGODB_URL: {}
+	MONGODB_DB: {
+		schema: building ? v.optional(v.string()) : v.string()
+	},
+
+	MONGODB_URL: {
+		schema: building ? v.optional(v.string()) : v.string()
+	}
 });
